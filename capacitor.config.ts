61617b8a1a2fd7a3e83f2b215c095fe5a8a4a91d
Maxime AMILLASTRE <maxime.amillastre@orange.com>
2023-11-09 11:25:05 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-standalone-icon-error',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
