53769e32cfd74c8556503759437b137a8f1dda7f
Maxime AMILLASTRE <maxime.amillastre@orange.com>
2023-11-09 11:37:02 +0100

    add the icons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon],
 })
 export class HomePage {
-  constructor() {}
+  constructor() { }
 }
